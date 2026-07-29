1a9e7a42884a8a62ce1da32a6ca2c2cf0f73a023
mudzivirievans <66200993+mudzivirievans@users.noreply.github.com>
2026-07-29 17:42:16 +0200

    load desktop service structure

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./AppRefined.tsx";
 import "./index.css";
 import "./skills-education-refinement";
+import "./desktop-layout-refinement";
 import { BrowserRouter } from "react-router-dom";
 
 const basename = import.meta.env.BASE_URL;
